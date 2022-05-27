476e1999f6f7d873a1375e0176315a89c5dc5bbf
David Bielik <davidbielik@gmail.com>
2022-05-27 07:41:11 -0400

    user and events classes working, basic association

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    # Redirests signing out users back to sign-in
+    get "users", to: "devise/sessions#new"
+  end
+  
+  devise_for :users
+  resources :events
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "events#index"
 end
