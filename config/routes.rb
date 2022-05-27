45277353483cbf5b5db9611bbc14599e52bf9150
David Bielik <davidbielik@gmail.com>
2022-05-27 09:52:18 -0400

    created users controller and show page

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   
   devise_for :users
   resources :events
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   
